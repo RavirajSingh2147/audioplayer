383df687648cc2e123a99504034364220d1160fa
Raviraj Singh Sisodiya <99286116+RavirajSingh2147@users.noreply.github.com>
2023-01-17 20:12:51 +0530

    Add files via upload

---
+++ audioplayer.py
@@ -0,0 +1,21 @@
+from playsound import playsound
+print('''TOP 2022 BEST RINGTONE 
+1: BABEL TRAP
+2: PERCOCET
+3: PIKACHU
+4: PINKSOLDIER
+4: RESURRECTION
+5: SQUIDGAME''')
+order=(input('ENTER WHICH YOU WANT TO PLAY: '))
+if"BABEL" in order:
+    playsound('BabeL.mp3')
+elif "PERCOCET" in order:
+        playsound('Percocet.mp3')
+elif "PIKACHU" in order:
+        playsound('Pikachu.mp3')        
+elif "PINKSOLDIER" in order:
+        playsound('Pink.mp3')        
+elif "RESURRECTION" in order:
+        playsound('Resur.mp3')        
+elif "SQUIDGAME" in order:
+        playsound('Squid.mp3')        
